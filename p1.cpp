596d24283ec232e11279d5abae2b094b67562642
Yangzhen0000 <616366255@qq.com>
2015-03-29 19:24:10 +0800

    Update p1.cpp

--- p1.cpp
+++ p1.cpp
@@ -7,20 +7,20 @@
 using namespace std;
 
 int main()
-{    int random_num[3], user_guess_num[3];                                      //two arrays to contain the random number and user guess number respectively
-	 int play_coun = 0, win = 0, lose = 0, all_right, num_right, time_coun;     //time recorders
+{    int random_num[3], user_guess_num[3];                                   //two arrays to contain the random number and user guess number respectively
+	 int play_coun = 0, win = 0, lose = 0, all_right = 0, num_right = 0, time_coun = 0;     //time recorders
      char choice;                                                               //decide play the game or not
 
      while (true){
 	 
 	     srand(time(NULL));                                                     //random number seed initialized
      
-		 cout << "Do you want to play the game_guess_number£¨Y/N£©?" << endl; 
+		 cout << "Do you want to play the game_guess_number（Y/N）?" << endl; 
 		 cin >> choice;
          
          time_coun = 0;
 		 if (choice == 'N'){
-             cout << "You have totally played" << play_coun << "time(s), and you have won " << win << " time(s), lost " << lose << " time(s)¡£" << endl;
+             cout << "You have totally played " << play_coun << " time(s), and you have won " << win << " time(s)，lost " << lose << " time(s)。" << endl;
              break;
          }                                                                                 //put out the result when end the game
 		 if (choice == 'Y'){
@@ -35,19 +35,25 @@ int main()
 		 	cout << random_num[0] << random_num[1] << random_num[2];
 	 	 	
 			while (true) {
-		     	cout << "Please input three different numbers you guess£º" << endl;        
+		     	cout << "Please input three different numbers you guess：" << endl;        
      	     	all_right = 0, num_right = 0, time_coun += 1;
-             
-			 	for (int j = 0; j < 3; ++j){  
-				     cin >> user_guess_num[j];
-					 if (user_guess_num[j] == random_num[j])    
-                     	all_right += 1;
-                	 else for (int i = 0; i < 3; ++i){
-                         	 if (i == j)    continue;
-							 if (user_guess_num[j] == random_num[i])
-                	         	num_right += 1;
-				           	}
-	          	}                                                                          //get in the user-guess number, compare and register
+     	     	
+     	     	bool flag = true;
+				do{
+				    cin >> user_guess_num[0] >> user_guess_num[1] >> user_guess_num[2];
+     	     	    if (cin.fail()){
+				        flag = false;
+					    cout << "Your inputs are ilegal, please input again:\n";
+				        cin.clear();
+				        cin.sync();
+				    }
+				    else flag = true;
+				}while (flag == false);                                                    //check the inputs, avoid dead line
+			 	
+				num_right = (user_guess_num[0] == random_num[1]) + (user_guess_num[0] == random_num[2]) + (user_guess_num[1] == random_num[0])
+				  + (user_guess_num[1] == random_num[2] ) + (user_guess_num[2] == random_num[0]) + (user_guess_num[2] == random_num[1]);                                                                     
+				all_right = (user_guess_num[0] == random_num[0]) + (user_guess_num[1] == random_num[1]) + (user_guess_num[2] == random_num[2]);
+				                                                                           //get in the user-guess number, compare and register
 	         	if (all_right == 3){
 	         		win += 1;
 	         		cout << "Congratulations! You win!" << endl; 
@@ -67,4 +73,3 @@ int main()
 	}
 	return 0;
 }
- 
